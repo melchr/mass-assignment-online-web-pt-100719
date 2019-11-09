a5dfd0506b5986ffa93029fbf4046f970dc79e84
Melissa Christman <melchrstmn@gmail.com>
2019-11-09 21:59:24 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,8 +2,8 @@ class Person
   
   attr_accessor :name, :hair_color
   
-  def initialize(name:, hair_color:)
-    
+def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end
   
 end
